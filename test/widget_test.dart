46e45418e43a2a51699b9c4028991c4c8ac78842
unknown <khusnul.kh783@gmail.com>
2026-04-05 22:03:42 +0700

    fix in pubspec

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tubes_mobile/main.dart';
+import 'package:LapanginAja/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
